7e73a2890186349d1837207a3b1ca0a8ef6c50a3
Vikash <vickyrajput9601@gmail.com>
2023-08-27 20:49:00 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+
+
+    name='mlproject',
+    version='0.01',
+    author='vikash',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','matplotlib','seaborn']
+)
